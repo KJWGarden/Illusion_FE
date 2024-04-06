35efd00d390c867357a05eed70f8bb32ada4159b
queenly9 <queenly9@naver.com>
2024-04-07 03:12:54 +0900

    게시판 초입 페이지 구현

---
+++ src/pages/PostListPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from "../components/Navbar.jsx";
+
+function PostListPage(props) {
+    return (
+        <>
+            <Navbar />
+            <div>
+                <img src="https://post-phinf.pstatic.net/MjAyMTA2MDNfMTUx/MDAxNjIyNjg3NjU3NDE2.vLf83uJlSwbb1S_bg1juckTGpspocycfDobem93TGVYg.bewA41NI-qpLnevFBJ0O5g0isODkX2K5_DO0gmC1Pq8g.JPEG/IMG_3083.jpg?type=w800_q75" />
+            </div>
+            );
+        </>
+    )
+}
+
+export default PostListPage;
